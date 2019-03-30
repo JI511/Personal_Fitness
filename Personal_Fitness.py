62012d42133055562131b821232b368f637e7143
John Ingwersen <johni@iastate.edu>
2019-03-29 19:30:02 -0500

    First commit

---
+++ Personal_Fitness.py
@@ -0,0 +1,15 @@
+###################################################################
+###              Personal Fitness Application                   ###
+###            Store and track progress for multiple            ###
+###            different fitness areas. Current plans           ###
+###            are body weight, calories, and lifted            ###
+###            weights.                                         ###
+###################################################################
+
+
+def weight_proc():
+    print("Weight Calculations.")
+
+
+if __name__ == '__main__':
+    weight_proc()
