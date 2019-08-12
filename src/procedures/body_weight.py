013eb5c01def57349ed11bb7d24d8971c33c4494
John Ingwersen <johni@iastate.edu>
2019-08-11 20:44:55 -0500

    Added plotting for body weight over time. Saving needs to be fixed.

--- src/procedures/body_weight.py
+++ src/procedures/body_weight.py
@@ -7,9 +7,10 @@ from Util import database_api as db_api
 from Util import constants
 from Util import Utilities as util
 
+table = 'body_weight'
+
 
 def add_new_data():
-    table = 'body_weight'
     while True:
         weight_text = input("What did you weigh today?\n"
                             "Optional: Input a file path to add multiple values (.txt)\n")
@@ -25,7 +26,6 @@ def add_new_data():
     db_api.create_table(constants.database_path, table, constants.body_weight_query)
     if not isinstance(result, list):
         result = [result]
-    print(result)
     for item in result:
         unique_id = db_api.add_new_row(constants.database_path, table)
         values = (item, unique_id)
@@ -33,6 +33,9 @@ def add_new_data():
         db_api.update_item(constants.database_path, table, values, names)
     db_api.get_table_rows(constants.database_path, table)
 
+
+def view_data():
+    util.plot_data(constants.database_path, table, ['body_weight'])
 
 # ----------------------------------------------------------------------------------------------------------------------
 #    End
