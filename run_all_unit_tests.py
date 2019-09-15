fc60d86576fabdbf5ac32d23d2ba89bd2c08f774
John Ingwersen <johningwersen11@gmail.com>
2019-09-15 09:52:05 -0500

    Updated to find all unit test names dynamically
    Made sure that -t command still works as well.

--- run_all_unit_tests.py
+++ run_all_unit_tests.py
@@ -4,16 +4,8 @@
 
 # imports
 import unittest
-import os
-import datetime
 import argparse
 
-from tests.Test_Help import suite
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", help='Specify the tests to run by test name.')
-args = parser.parse_args()
-name = args.t if args.t is not None else None
 print("##########################################################\n"
       "######                                              ######\n"
       "######      ##    ##  ##    ##   ##   ######        ######\n"
@@ -27,52 +19,14 @@ print("##########################################################\n"
       "######                                              ######\n"
       "##########################################################\n")
 
-log_file = os.path.join(os.getcwd(), "unit_test_logs.txt")
-fail_list = []
-error_list = []
-with open(log_file, "w") as file:
-    file.write(str(datetime.datetime.now()))
-    runner = unittest.TextTestRunner(file)
-    for test in suite(name):
-        result = runner.run(test)
-        if result.wasSuccessful():
-            print("PASS\t%s" % str(test))
-        else:
-            print("FAIL\t%s" % str(test))
-            if len(result.failures) != 0:
-                fail_list.append(str(test))
-                for failure in result.failures[0]:
-                    print(failure)
-            if len(result.errors) != 0:
-                error_list.append(str(test))
-                for error in result.errors[0]:
-                    print(error)
-if len(fail_list) == 0 and len(error_list) == 0:
-    print("\n##################\n"
-          "#                #\n"
-          "#    All tests   #\n"
-          "#   successful!  #\n"
-          "#                #\n"
-          "##################")
-else:
-    print("\n    ###\n"
-          "    ###\n"
-          "    ###\n"
-          "    ###        Errors!\n"
-          "    ###\n"
-          "    ###\n"
-          "\n"
-          "    ###\n"
-          "    ###\n")
-    if len(fail_list) != 0:
-        print('\nFailures:')
-        for f in fail_list:
-            print(f)
-    if len(error_list) != 0:
-        print('\nErrors:')
-        for e in error_list:
-            print(e)
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", help='Specify the tests to run by test name.')
+args = parser.parse_args()
+name = 'test_' + args.t + '*.py' if args.t is not None else 'test*.py'
 
+loader = unittest.TestLoader()
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(loader.discover(start_dir='tests', pattern=name))
 
 # ----------------------------------------------------------------------------------------------------------------------
 #    End
